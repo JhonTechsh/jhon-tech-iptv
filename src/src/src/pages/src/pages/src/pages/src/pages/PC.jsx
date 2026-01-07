c945715dbd5310288cc4879e3581cb403d663ed7
JhonTechsh <suportejhontech@gmail.com>
2026-01-07 20:57:29 -0300

    Delete src/src directory

--- src/src/src/pages/src/pages/src/pages/src/pages/PC.jsx
+++
@@ -1,8 +0,0 @@
-export default function PC() {
-  return (
-    <section style={{ padding: 40 }}>
-      <h2>IPTV no PC / Notebook</h2>
-      <p>Use VLC ou IPTV Smarters para Windows e Mac.</p>
-    </section>
-  );
-}
