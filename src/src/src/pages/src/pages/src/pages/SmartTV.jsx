421e646987d75c98d4127fa4ba21199838c8f4a6
JhonTechsh <suportejhontech@gmail.com>
2026-01-07 20:42:41 -0300

    Create SmartTV.jsx

---
+++ src/src/src/pages/src/pages/src/pages/SmartTV.jsx
@@ -0,0 +1,12 @@
+export default function SmartTV() {
+  return (
+    <section style={{ padding: 40 }}>
+      <h2>IPTV em Smart TV</h2>
+      <p>Samsung (Tizen) e LG (WebOS)</p>
+      <ul>
+        <li>Instale IPTV Smarters</li>
+        <li>Faça login com dados Xtream</li>
+      </ul>
+    </section>
+  );
+}
